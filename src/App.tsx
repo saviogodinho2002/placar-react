33ab4e2983159394ae94b6570a4005b0722e2f34
saviogodinho <saviogodinho2002@gmai.com>
2022-05-04 22:38:34 -0300

    bug-fix

--- src/App.tsx
+++ src/App.tsx
@@ -45,16 +45,18 @@ function App() {
       <button id="create-placar-button" className='placar-button' onClick={addPlacar}> + </button>
       <button id="download-json-button" className='placar-button' onClick={gerateJson}> v </button>
 
-      <input id='input-json' type="file" accept='.json' onChange={(e)=>{ 
-        
-        const file = e.target.files[0] ;
+      <input id='input-json' type="file" accept='.json' onChange={(e?)=>{ 
 
-        if(file == null) return;
+         if(e.target.files == null) return;
+        
+        let file = e.target.files[0]  ;
+
+        
 
         const reader = new FileReader();
 
         reader.onload = function(event){
-         setPlacares( JSON.parse(event.target?.result))
+         setPlacares( JSON.parse(event.target?.result as string))
         }
         reader.readAsText(file);
         }} />
